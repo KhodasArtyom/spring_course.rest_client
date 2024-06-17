68fe352f9e913a69eff97149ba3cb4e2875e8971
ArtyomKhodas <a.Khodas1601@gmail.com>
2024-06-17 13:59:51 +0300

    first_commit

---
+++ src/main/java/com/artemhodas/spring/rest/App.java
@@ -0,0 +1,13 @@
+package com.artemhodas.spring.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ src/main/java/com/artemhodas/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package com.artemhodas.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.artemhodas.spring.rest")
+public class MyConfig {
+}
